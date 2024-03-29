a15428bb643e2d252b0ac9928e0f48e1de494ff4
Marisa Saunders <marisaksaunders@gmail.com>
2024-03-28 22:50:36 -0400

    styled book card component

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './BooksStyles.css'
 
 function Book({ book }) {
   return (
-    <div>
-      <h3>{book.title}</h3>
+    <div className="book-card">
+      <img src={book.cover} className="book-cover"/>
+      <h2 className="book-title">{book.title}</h2>
+      <p className="book-author">Author: {book.author}</p>
     </div>
   )
 }
--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -4,14 +4,13 @@ import Book from './Book';
 
 function Books() {
   const [books, setBooks] = useState([
-    { id: 1, title: "Eloquent Javascript", author: "Marjin Haverbeke", cover: './images/eloquent.jpg' },
-    { id: 2, title: "Grokking Algorithms", author: "Aditya Y. Bhargava", cover:'./images/grokking.jpg' },
-    { id: 3, title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz", cover: './images/ruby.jpg' }
+    { id: 1, title: "Eloquent Javascript", author: "Marjin Haverbeke", cover: 'https://m.media-amazon.com/images/I/81HqVRRwp3L._AC_UF1000,1000_QL80_.jpg' },
+    { id: 2, title: "Grokking Algorithms", author: "Aditya Y. Bhargava", cover:'https://m.media-amazon.com/images/I/81BdMC18EUL._AC_UF1000,1000_QL80_.jpg' },
+    { id: 3, title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz", cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1358916543i/13507787.jpg' }
   ]);
 
   return (
     <div className="book-container">
-      <h3>Books</h3>
       {books.map(book => (
         <Book key={book.id} book={book}/>
       ))}
